94250ccdcdbe6d62236c8fd1912596f07cc87e34
pabloalarconmanzano <pabloalarconmanzano@gmail.com>
2022-11-14 15:58:38 +0100

    creacion ejercicio1 js

---
+++ js/ejercicio1.js
@@ -0,0 +1,17 @@
+console.info("MENSAJE DE INFO")
+console.warn("MENSAJE DE WARNING")
+console.error("MENSAJE DE ERROR");
+console.log("MENSAJE DE LOG");
+console.time();
+console.timeEnd();
+
+
+
+var numero1 = 58;
+var numero2 = 44;
+var suma = numero1 + numero2;
+var resta = numero1 - numero2;
+
+console.log("La suma de los numeros " + numero1 + " y " + numero2 + " es: " + suma );
+console.log("La resta de los numeros " + numero1 + " y " + numero2 + " es: " + resta );
+console.log("", 1, {}, [3,4,7]);
