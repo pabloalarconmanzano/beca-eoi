b223910bda0b63f35f4442a0a6ff0c3cf441783d
pabloalarconmanzano <pabloalarconmanzano@gmail.com>
2022-11-14 18:38:58 +0100

    creacion ejercicio6

---
+++ js/ejercicio5.js
@@ -0,0 +1,26 @@
+var array1 = [1,2,3,4];
+
+
+for(var i=0; i < array1.length; i++){
+    switch (array1) {
+        case 1:
+            console.log(array1(1));
+            break;
+        case 2:
+            console.log(array1(2));
+            break;
+        case 3:
+            console.log(array1(3));
+            break;
+        case 4:
+            console.log(array1(4));
+            break;
+    }
+}
+
+
+
+array1.push(3);
+console.log(array1);
+
+
---
+++ js/ejercicio6.js
@@ -0,0 +1,8 @@
+var array1 = [1,2,3,4];
+
+
+for(var i = 0; i < 5; i++){
+    console.log(i);
+}
+
+
